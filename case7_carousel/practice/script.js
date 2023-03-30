47e88364042504bfcbb94e43d34f86b84da8c4a8
jinWan0823 <dhksl823@gmail.com>
2023-03-30 12:07:17 +0900

    carousel 슬라이드

--- case7_carousel/practice/script.js
+++ case7_carousel/practice/script.js
@@ -6,82 +6,92 @@
   }
 
   class Carousel {
-    constructor(caruoselElement) {
-      this.caruoselElement = caruoselElement
-      this.itemClassName = 'carousel_item'
-      this.items = this.caruoselElement.querySelectorAll('.carousel_item')
-      this.totalItems = this.items.length
-      this.current = 0
+    constructor(carouselElement) {
+        this.carouselElement = carouselElement;
+        this.itemClassName = 'carousel_item';
+        this.items = this.carouselElement.querySelectorAll('.carousel_item');
+
+        this.totalItems = this.items.length;
+        this.current = 0;
+        this.isMoving = false;
     }
 
-    initCarousel() {
-      this.items[this.totalItems - 1].classList.add('prev')
-      this.items[0].classList.add('active')
-      this.items[1].classList.add('next')
+    initCarousel(){
+        this.isMoving = false;
+        this.items[0].classList.add('active');
+        this.items[1].classList.add('next');
+        this.items[this.totalItems - 1].classList.add('prev');
     }
 
-    moveCarouselTo() {
-      let prev = this.current - 1
-      let next = this.current + 1
+    disableInteraction(){
+        this.isMoving = true;
+        setTimeout(()=>{
+            this.isMoving = false;
+        }, 500)
+    }
 
-      if (this.current === 0) {
-        prev = this.totalItems - 1
-      } else if (this.current === this.totalItems - 1) {
-        next = 0
-      }
 
-      for (let i = 0; i < this.totalItems; i++) {
-        if (i === this.current) {
-          this.items[i].className = this.itemClassName + ' active'
-        } else if (i === prev) {
-          this.items[i].className = this.itemClassName + ' prev'
-        } else if (i === next) {
-          this.items[i].className = this.itemClassName + ' next'
-        } else {
-          this.items[i].className = this.itemClassName
+    setEventListener(){
+        this.prevButton = this.carouselElement.querySelector('.carousel_button--prev');
+        this.nextButton = this.carouselElement.querySelector('.carousel_button--next');
+        
+        this.prevButton.addEventListener('click',()=>{
+            this.movePrev();
+        })
+        this.nextButton.addEventListener('click',()=>{
+            this.moveNext();
+        })
+    }
+    moveCarouselTo(){
+        if(this.isMoving) return
+        this.disableInteraction();
+        let prev = this.current -1;
+        let next = this.current +1;
+
+        if(this.current === 0){
+            prev = this.totalItems -1;
+        } else if (this.current === this.totalItems -1){
+            next = 0;
         }
-      }
+
+        for(let i = 0;i<this.totalItems; i++){
+            if(i === this.current) {
+                this.items[i].className = this.itemClassName + ' active';
+            } else if (i ===prev){
+                this.items[i].className = this.itemClassName + ' prev';
+            } else if (i===next){
+                this.items[i].className = this.itemClassName + ' next';
+            } else{
+                this.items[i].className = this.itemClassName;
+            }
+        }
+
     }
-
-    movePrev() {
-      if (this.current === 0) {
-        this.current = this.totalItems - 1
-      } else {
-        this.current--
-      }
-      this.moveCarouselTo()
+    moveNext(){
+        if(this.isMoving) return
+        if(this.current === this.totalItems -1){
+            this.current = 0;
+        } else{
+            this.current++;
+        }
+        this.moveCarouselTo();
     }
-
-    moveNext() {
-      if (this.current === this.totalItems - 1) {
-        this.current = 0
-      } else {
-        this.current++
-      }
-      this.moveCarouselTo()
-    }
-
-    setEventListeners() {
-      this.prevButton = this.caruoselElement.querySelector(
-        '.carousel_button--prev'
-      )
-      this.nextButton = this.caruoselElement.querySelector(
-        '.carousel_button--next'
-      )
-
-      this.prevButton.addEventListener('click', () => {
-        this.movePrev()
-      })
-      this.nextButton.addEventListener('click', () => {
-        this.moveNext()
-      })
+    movePrev(){
+        if(this.isMoving) return
+        if(this.current === 0){
+            this.current = this.totalItems -1;
+        } else {
+            this.current--;
+        }
+        this.moveCarouselTo();
     }
   }
 
   document.addEventListener('DOMContentLoaded', () => {
-    const carouselElement = get('.carousel')
-    const carousel = new Carousel(carouselElement)
-    carousel.initCarousel()
-    carousel.setEventListeners()
+    const carouselElement = get('.carousel');
+    const carousel = new Carousel(carouselElement);
+
+    carousel.initCarousel();
+    carousel.setEventListener();
   })
 })()
