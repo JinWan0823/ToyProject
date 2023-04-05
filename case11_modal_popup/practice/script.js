4704b8a3f6c6bd7edbb89bb8e1b96f26b658c088
jinWan0823 <dhksl823@gmail.com>
2023-04-05 10:14:16 +0900

    모달 팝업 개발

--- case11_modal_popup/practice/script.js
+++ case11_modal_popup/practice/script.js
@@ -3,4 +3,36 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  const $button = get('.modal_open_button');
+  const $body = get('body');
+  const $modal = get('.modal');
+  const $modalCancleButton = get('.modal_button.cancel');
+  const $modalConfirmButton = get('.modal_button.confirm');
+
+
+  const toggleModal = () => {
+    $body.classList.toggle('scroll_lock');
+    $modal.classList.toggle('show');
+  }
+
+  $button.addEventListener('click', () => {
+    toggleModal();
+  })
+
+  $modalConfirmButton.addEventListener('click', () => {
+    toggleModal();
+    console.log('confirmed!');
+  })
+
+  $modalCancleButton.addEventListener('click', () => {
+    toggleModal();
+  })
+
+  window.addEventListener('click', (e) => {
+    if(e.target === $modal) {
+      toggleModal();
+    }
+  })
+
 })()
